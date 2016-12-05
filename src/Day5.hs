0a360a496b1a0c7a7882a0bd0c86e5fb5eae6e18
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2016-12-05 17:40:28 +0100

    [Day 5]

---
+++ src/Day5.hs
@@ -0,0 +1,48 @@
+module Day5 where
+
+import Test.Hspec
+
+import Data.Hash.MD5
+import Data.List (isPrefixOf, find)
+import Safe (readMay)
+
+-- Problem DSL
+ids idx = map (\x -> idx ++ show x) [0..]
+allMd5 idx = map (\x -> (x, (md5s . Str) x)) (ids idx)
+
+isChar x = "00000" `isPrefixOf` x
+-- utils
+
+allChars idx = filter (isChar . snd) (allMd5 idx)
+
+-- FIRST problem
+day code = let
+  codes = allChars code
+  chars = map ((!!5) . snd) codes
+
+  in chars
+
+-- SECOND problem
+day' code = let
+  codes = allChars code
+  in map (findOffset (map snd codes)) [0..7]
+
+findOffset :: [String] -> Int -> Char
+findOffset l i = let Just s = find (\x -> case readMay (x !! 5 : []) of
+                                       Just i' -> i == i'
+                                       Nothing -> False)
+                                     l
+                 in s !! 6
+
+-- tests and data
+
+test = hspec $ do
+  describe "firstProblem" $ do
+    it "dosometthing" $ do
+      day "abc" `shouldBe` "18f47a30"
+      day content `shouldBe` "f97c354d"
+
+      day' "abc" `shouldBe` "05ace8e3"
+      day' content `shouldBe` "863dde27"
+
+content = "reyedfim"
